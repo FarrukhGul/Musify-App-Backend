90f88a80800aa3b97661098e092d8b7627a5d5ef
FarrukhGul <coder.gulfarrukh@gmail.com>
2026-02-26 01:26:21 +0500

    Fix CORS for all devices

--- src/app.js
+++ src/app.js
@@ -7,26 +7,10 @@ const cors = require('cors');
 const app = express();
 
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://musify-app-frontend.vercel.app',
-  'https://musify-app-frontend-9aq25vmfo-farrukhs-projects-2b8a84ac.vercel.app' 
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
- 
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: true,  
   credentials: true
 }));
-
 
 app.use(express.json());
 app.use(cookieParser());
