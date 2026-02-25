a08da778e3e864d28f6c27096fc99eb2eeedca2b
FarrukhGul <coder.gulfarrukh@gmail.com>
2026-02-26 01:14:10 +0500

    Fix CORS configuration order

--- src/app.js
+++ src/app.js
@@ -2,21 +2,37 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('../src/routes/auth.routes')
 const musicRoutes = require('../src/routes/music.routes')
-
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
-app.use(express.json())
-app.use(cookieParser())
 
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://musify-app-frontend.vercel.app',
+  'https://musify-app-frontend-9aq25vmfo-farrukhs-projects-2b8a84ac.vercel.app' 
+];
 
 app.use(cors({
-  origin: 'http://localhost:5173',  // your React frontend
-  credentials: true                  // allows cookies to be sent
-}))
+  origin: function (origin, callback) {
+ 
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
-app.use('/api/auth', authRoutes)
-app.use('/api/music', musicRoutes)
 
-module.exports = app
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/music', musicRoutes);
+
+module.exports = app;
